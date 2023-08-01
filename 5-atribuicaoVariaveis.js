d69a1bee5fd925fea1ebaf7fca2efd13023e05db
Maycon2023 <127303829+Maycon2023@users.noreply.github.com>
2023-08-01 11:46:55 -0300

    Add files via upload

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,7 @@
+console.log("Trabalahando com atribuição de variáveis");
+const idade = 29;
+const nome = "Ricardo";
+const sobrenome = "bugan";
+
+//console.log(nome + " " + sobrenome)
+console.log(nome, sobrenome)

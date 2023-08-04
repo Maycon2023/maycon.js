1fcbe6c6b228af137ef02cfa3888060fad00bec7
Maycon2023 <127303829+Maycon2023@users.noreply.github.com>
2023-08-04 09:06:32 -0300

    Add files via upload

--- 5-atribuicaoVariaveis.js
+++ 5-atribuicaoVariaveis.js
@@ -4,4 +4,8 @@ const nome = "Ricardo";
 const sobrenome = "bugan";
 
 //console.log(nome + " " + sobrenome)
-console.log(nome, sobrenome)
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}´);
+contador = contador +1
+nome = nome + sobrenome;
+console.log(nome);

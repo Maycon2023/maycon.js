65747072f2c6ec2a416dc1f0050ef9a5aa13b7ce
Maycon2023 <127303829+Maycon2023@users.noreply.github.com>
2023-08-16 09:51:47 -0300

    Add files via upload

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -5,23 +5,57 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 );
 
-const idadeComprador = 17
-console.log("Destinos possíveis:");
-console.log(listaDeDestinos);
+const idadeComprador = 17;
+const estaAcompanhada = false;
+const temPassagemComprada = true;
 
-if(maiorDeIdade >= 18){
+// if(idadeComprador >= 18){
+//   console.log("Comprar maior de idade");
+//    listaDeDestinos.splice(1,1); // removendo item
+// }else if(estaAcompanhada == true){
+//       console.log("Comprador está acompanhado");
+//       listaDeDestinos.splice(1,1); // removendo item
+// }else {
+//       console.log("Não é maior de idade e não vender ");
+// } 
+
+
+if(idadeComprador >= 18 || estaAcompanhada == true) {
     console.log("Comprar maior de idade");
-    listaDeDestinos.splice(1,1); // removendo item
-}else{
-    //A pessoa é menor de idade
+    listaDeDestinos.splice(2,1); // removendo item
+}else {
     console.log("Não é maior de idade e não vender ");
+} 
+
+console.log("Embarque: \n\n");
+if(idadeComprador >= 18 && temPassagemComprada){
+    console.log("Boa viagem");
+}else{
+    console.log("Você  não pode embarcar");
 }
 
 
+
+
 console.log(listaDeDestinos);
 
-console.log(idadeComprador > 18);
-console.log(idadeComprador < 18);
-console.log(idadeComprador <= 18);
-console.log(idadeComprador >= 18);
-console.log(idadeComprador == 18);
+// console.log(idadeComprador > 18);
+// console.log(idadeComprador < 18);
+// console.log(idadeComprador <= 18);
+// console.log(idadeComprador >= 18);
+// console.log(idadeComprador == 18);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Bom dia
